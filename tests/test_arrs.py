0e33df11273e6789b84b90ebe838af565062f3cb
PolinaK <pklopunova@gmail.com>
2023-02-24 13:32:49 +0600

    add bug/issue1

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -8,10 +8,11 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
         self.assertEqual(arrs.get([], 0, "test"), "test")
         self.assertEqual(arrs.get([1, 2, 3], -1), None)
+       # self.assertEqual(arrs.get([], 10), None)
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], -1, 1), [])
-
+        self.assertEqual(arrs.my_slice([], 1, None), [])
 
